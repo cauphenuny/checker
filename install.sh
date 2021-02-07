78ddfdd3181ce3df27e0df26a142ce5e21e7a8f1
ycpedef <ycpedef>
2021-02-07 23:10:47 +0800

    2021-02-07_23:10:47

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@ if [ -f  /bin/checker ]; then
     echo "Warning: rewrite /bin/checker"
 fi
 rm -rf  ~/.ycpedef_checker_source/
-git clone https://github.com/ycpedef/checker.git ~/.ycpedef_checker_source/
+cp -r ./ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
+git pull origin master
+
+wget -c ftp://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz 
+tar -zxvf readline-6.2.tar.gz 
+cd readline-6.2
+./configure
+make && make installl
+ldconfig
+
 sh ./compile.sh
