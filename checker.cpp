c807dbefa10d1caec6830ab059a9dc7ba822805c
ycpedef <ycpedef>
2021-02-07 23:32:11 +0800

    2021-02-07_23:32:11

--- checker.cpp
+++ checker.cpp
@@ -11,11 +11,18 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include "color.h"
-#include "readline/history.h"
-#include "readline/readline.h"
+//#include "readline/history.h"
+//#include "readline/readline.h"
 using namespace std;
 
 string __version = "checker v5.2";
+
+string readline(string prompt) {
+    printf("%s", prompt.c_str());
+    string res;
+    cin >> res;
+    return res;
+}
 
 long long myclock() {
     struct timeval tv;
--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 if [ -f  /bin/checker ]; then
     echo "Warning: rewrite /bin/checker"
 fi
+
+sudo apt install  libreadline-dev git
+
 rm -rf  ~/.ycpedef_checker_source/
 cp -r ./ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 git pull origin master
-
-wget -c ftp://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz 
-tar -zxvf readline-6.2.tar.gz 
-cd readline-6.2
-./configure
-make && make installl
-ldconfig
 
 sh ./compile.sh
