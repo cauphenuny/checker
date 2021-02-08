88e43eb4d0eabcf49eba06b7bee1a2eb07af0924
ycpedef <854261863@qq.com>
2021-02-08 15:01:36 +0800

    2021-02-08_15:01:36

--- forced_update.sh
+++ forced_update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+echo "delete file ..."
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
+echo "download file ..."
 git init 1> /dev/null 2>&1
 git remote add origin https://github.com/ycpedef/checker 1> /dev/null 2>&1
 git pull origin master 1> /dev/null 2> /dev/null
+echo "move file ..."
 killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
