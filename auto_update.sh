23a303a916990d304dcfb60791f97242ed4b5558
ycpedef <854261863@qq.com>
2021-02-08 21:50:40 +0800

    2021-02-08 21:50:40

--- auto_update.sh
+++ auto_update.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-killall checker
+# echo "delete file ..."
+if [ -f ~/.ycpedef_checker_buffer ]; then
+    rm -rf ~/.ycpedef_checker_buffer/
+fi
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
-cd ~/.ycpedef_checker_update
-if [ -f ./master.zip ]; then
-    rm -rf ./master.zip
-fi
-if [ -f ./checker/ ]; then
-    rm -rf ./checker/
-fi
-wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip 2>/dev/null 1>&2
-unzip master.zip 2>/dev/null 1>&2
-cp -r checker/ ~/.ycpedef_checker_source
-cd ~/.ycpedef_checker_source
+mkdir -p ~/.ycpedef_checker_buffer
+
+# echo "download file ..."
+cd ~/.ycpedef_checker_buffer 1>/dev/null 2>&1
+wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip 1>/dev/null 2>&1
+unzip master.zip 1>/dev/null 2>&1
+cp -r checker/ ~/.ycpedef_checker_source 1>/dev/null 2>&1
+cd ~/.ycpedef_checker_source 1>/dev/null 2>&1
+
+# echo "move file ..."
+killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
 fi
-
--- forced_update.sh
+++ forced_update.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 echo "delete file ..."
+if [ -f ~/.ycpedef_checker_buffer ]; then
+    rm -rf ~/.ycpedef_checker_buffer/
+fi
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
+mkdir -p ~/.ycpedef_checker_buffer
 
 echo "download file ..."
-cd ~/.ycpedef_checker_update
+cd ~/.ycpedef_checker_buffer
 wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip
 unzip master.zip
 cp -r checker/ ~/.ycpedef_checker_source
@@ -15,4 +19,3 @@ killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
 fi
-
