29f9b22db0f055a1e33906ec2153d1fb8df08142
ycpedef <854261863@qq.com>
2021-02-08 10:34:38 +0800

    2021-02-08_10:34:38

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-killall -9 checker
+killall checker
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 git init
 git remote add origin https://github.com/ycpedef/checker
 git pull origin master 1> /dev/null 2> /dev/null
-bash ./copy.sh
+pwd
+#bash ./copy.sh
 
