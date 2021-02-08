99544fd5a69c2a10b1633775d121496d14f31e86
ycpedef <854261863@qq.com>
2021-02-08 21:41:25 +0800

    2021-02-08 21:41:25

--- auto_update.sh
+++ auto_update.sh
@@ -2,7 +2,10 @@
 killall checker
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
-git clone https://github.com/ycpedef/checker ~/.ycpedef_checker_source 2> /dev/null 1>&2
+cd ~/.ycpedef_checker_update
+wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip 2>/dev/null 1>&2
+unzip master.zip 2>/dev/null 1>&2
+cp -r checker/ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
--- forced_update.sh
+++ forced_update.sh
@@ -4,7 +4,10 @@ rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 
 echo "download file ..."
-git clone https://github.com/ycpedef/checker ~/.ycpedef_checker_source
+cd ~/.ycpedef_checker_update
+wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip
+unzip master.zip
+cp -r checker/ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 
 echo "move file ..."
@@ -12,3 +15,4 @@ killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
 fi
+
