6079e78691ace1e4cea832b3e00453aaab94ca2e
ycpedef <854261863@qq.com>
2021-02-08 15:24:15 +0800

    2021-02-08_15:24:15

--- auto_update.sh
+++ auto_update.sh
@@ -4,7 +4,7 @@ rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 git init 1> /dev/null 2>&1
-git remote add origin https://gitee.com/yuanchenpu/checker 1> /dev/null 2>&1
+git remote add origin https://gitee.com/ycpedef/checker 1> /dev/null 2>&1
 git pull origin master 1> /dev/null 2> /dev/null
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
