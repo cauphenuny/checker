6f91bacdd8c60568c6d7846d6000536dabcdb76a
ycpedef <ycpedef@foxmail.com>
2021-02-08 23:23:28 +0800

    2021-02-08 23:23:28

--- upload.sh
+++ upload.sh
@@ -4,5 +4,5 @@ git add .
 date +"%F %T" | xargs -d'\n' -t git commit -m
 echo "git push -u gitee master"
 git push -u gitee master
-echo "git push -u origin master"
-git push -u origin master
+# echo "git push -u origin master"
+# git push -u origin master
