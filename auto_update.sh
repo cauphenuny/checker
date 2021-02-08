6dd86d62845efaee0204ab90a1824749dce51912
ycpedef <854261863@qq.com>
2021-02-08 21:45:59 +0800

    2021-02-08 21:45:59

--- auto_update.sh
+++ auto_update.sh
@@ -3,6 +3,12 @@ killall checker
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_update
+if [ -f ./master.zip ]; then
+    rm -rf ./master.zip
+fi
+if [ -f ./checker/ ]; then
+    rm -rf ./checker/
+fi
 wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip 2>/dev/null 1>&2
 unzip master.zip 2>/dev/null 1>&2
 cp -r checker/ ~/.ycpedef_checker_source
