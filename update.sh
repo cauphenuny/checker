0dd0b26f1b2c4a3dc4e072faa00f448c710e12c0
ycpedef <854261863@qq.com>
2021-02-08 10:47:17 +0800

    2021-02-08_10:47:17

--- update.sh
+++ update.sh
@@ -6,6 +6,9 @@ cd ~/.ycpedef_checker_source
 git init 1> /dev/null 2>&1
 git remote add origin https://github.com/ycpedef/checker 1> /dev/null 2>&1
 git pull origin master 1> /dev/null 2> /dev/null
-pwd
-./copy.sh
+# pwd
+if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
+    ~/.ycpedef_checker_source/copy.sh
+fi
+
 
