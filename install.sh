ee988314b235839fef7e89f6bcafaf33a021a0f9
ycpedef <854261863@qq.com>
2021-02-08 11:56:14 +0800

    2021-02-08_11:56:14

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if [ -f ~/.ycpedef_checker_source ]; then
 fi
 
 echo "installing git ..."
-apt install -y git 1>/dev/null 2>/dev/null
+sudo apt install -y git 1>/dev/null 2>/dev/null
 
 echo "download file ..."
 cp -r ./ ~/.ycpedef_checker_source
@@ -27,8 +27,8 @@ cd ~/.ycpedef_checker_source
 git pull origin master 1>/dev/null 2>/dev/null
 
 echo "copy file ..."
-sh ./copy.sh
-chmod 777 /usr/local/bin/checker
+sudo sh ./copy.sh
+sudo chmod 777 /usr/local/bin/checker
 mkdir -p ~/.ycpedef_checker_update
 cp ~/.ycpedef_checker_source/update.sh ~/.ycpedef_checker_update/update.sh
-chmod +x ~/.ycpedef_checker_update/update.sh
+sudo chmod 777 ~/.ycpedef_checker_update/update.sh
