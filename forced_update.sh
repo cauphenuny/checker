19e1644685a794bf2322eb888d39b8e2d1f327b2
ycpedef <ycpedef@foxmail.com>
2021-02-09 11:32:14 +0800

    2021-02-09 11:32:14

--- forced_update.sh
+++ forced_update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "\e[0;32 delete file ... \e[0"
+printf "\e[0;32 delete file ... \e[0"
 rm -rf ~/.ycpedef_checker_buffer/
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_buffer
 
-echo "\e[0;32 download file ... \e[0"
+printf "\e[0;32 download file ... \e[0"
 cd ~/.ycpedef_checker_buffer
 wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip
 unzip master.zip
@@ -13,7 +13,7 @@ cd checker/
 cp -r ./ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
 
-echo "\e[0;32 move file ... \e[0"
+printf "\e[0;32 move file ... \e[0"
 killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
