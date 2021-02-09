526c1b16cbb0404f957bec65e1d1780ea26b5319
ycpedef <ycpedef@foxmail.com>
2021-02-09 14:56:39 +0800

    2021-02-09 14:56:39

--- auto_update.sh
+++ auto_update.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 # echo "delete file ..."
 killall checker 1>/dev/null 2>&1
+rm -rf ~/.ycpedef_checker_buffer/ 1>/dev/null 2>&1
 rm -rf ~/.ycpedef_checker_source 1>/dev/null 2>&1
 mkdir -p ~/.ycpedef_checker_source 1>/dev/null 2>&1
+mkdir -p ~/.ycpedef_checker_buffer 1>/dev/null 2>&1
 
 # echo "download file ..."
+cd ~/.ycpedef_checker_buffer 1>/dev/null 2>&1
+wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip 1>/dev/null 2>&1
+unzip master.zip 1>/dev/null 2>&1
+cd checker/
+cp -r ./ ~/.ycpedef_checker_source 1>/dev/null 2>&1
 cd ~/.ycpedef_checker_source 1>/dev/null 2>&1
-git clone https://github.com/ycpedef/checker ./ --depth=1 1>/dev/null 2>&1
 
 # echo "move file ..."
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
 fi
+
+rm -rf ~/.ycpedef_checker_buffer 1>/dev/null 2>&1
 
--- forced_update.sh
+++ forced_update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-echo -e "\e[1;32m[delete file] \e[0m"
+echo -e "\e[1;32m [delete file] \e[0m"
+rm -rf ~/.ycpedef_checker_buffer/
 rm -rf ~/.ycpedef_checker_source
 mkdir -p ~/.ycpedef_checker_source
+mkdir -p ~/.ycpedef_checker_buffer
 
-echo -e "\e[1;32m[download file] \e[0m"
+echo -e "\e[1;32m [download file] \e[0m"
+cd ~/.ycpedef_checker_buffer
+wget https://gitee.com/yuanchenpu/checker/repository/archive/master.zip
+unzip master.zip
+cd checker/
+cp -r ./ ~/.ycpedef_checker_source
 cd ~/.ycpedef_checker_source
-git clone https://github.com/ycpedef/checker ./ --depth=1
 
-echo -e "\e[1;32m[move file] \e[0m"
+echo -e "\e[1;32m [move file] \e[0m"
 killall checker
 if [ -f ~/.ycpedef_checker_source/copy.sh ]; then
     ~/.ycpedef_checker_source/copy.sh
 fi
+rm -rf ~/.ycpedef_checker_source 1>/dev/null 2>&1
