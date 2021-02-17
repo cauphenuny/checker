311b9a458e8174bc32f64944b2d0cac200514bba
ycpedef <854261863@qq.com>
2021-02-17 13:43:08 +0800

    2021-02-17 13:43:08

--- checker.cpp
+++ checker.cpp
@@ -245,7 +245,7 @@ void analysis_long_cmd(string s, int &pos) {
         } else if (value == "never") {
             save_mode = 3;
         } else {
-            printf("Invalid save mode " L_RED "\"%s\"" NONE "!\n", value.c_str());
+            printf("Invalid save mode " RED "\"%s\"" NONE " !\n", value.c_str());
             exit(1);
         }
     } else if (key == "branch") {
@@ -253,7 +253,7 @@ void analysis_long_cmd(string s, int &pos) {
         printf("changed branch to <%s>.\n", branch.c_str());
         start_update();
     } else {
-        printf("Invalid option " L_RED "\"%s\"" NONE "!\n", key.c_str());
+        printf("Invalid option " RED "\"%s\"" NONE " !\n", key.c_str());
         exit(1);
     }
 }
@@ -270,7 +270,7 @@ void analysis_cmd(string cmd)  {
             case 'h': usage(0); break;
             case '-': analysis_long_cmd(cmd, i), i--; break;
             default:
-                printf("Invalid option " L_RED "'%c'!\n" NONE, cmd[i]);
+                printf("Invalid option " RED "'%c'" NONE " !\n", cmd[i]);
                 exit(1);
                 break;
         }
