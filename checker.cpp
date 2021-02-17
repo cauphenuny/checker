c00accf335e8bb07f565bd0cc52f14d933037144
ycpedef <854261863@qq.com>
2021-02-17 13:43:46 +0800

    2021-02-17 13:43:46

--- checker.cpp
+++ checker.cpp
@@ -196,7 +196,8 @@ void start_forced_update() {
 
 void usage(int id) {
     puts("usage: ");
-    puts("\nchecker [$problem_name] [-vlcqfu] [--save=] [--branch=]\n");
+    puts("\nchecker [$problem_name] [-vlcqfuh] [--save=] [--branch=]\n");
+    puts("-h: display this help and quit\n");
     puts("-f: fast mode");
     puts("-l: always load problem file");
     puts("-c: always continue when error occurs");
