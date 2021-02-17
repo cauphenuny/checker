19de34452f8ef9cec6871df4966b988f07b1b752
ycpedef <854261863@qq.com>
2021-02-17 11:57:52 +0800

    2021-02-17 11:57:52

--- checker.cpp
+++ checker.cpp
@@ -19,7 +19,7 @@
 #include "color.h"
 using namespace std;
 
-string __version = "checker v5.6.0";
+string __version = UNDERLINE "checker v5.6.0" NONE;
 string __branch = " <dev>";
 
 //string readline(string prompt) {
@@ -135,7 +135,7 @@ void quit(int signum) {
     printf(L_BLUE"Time Limit Exceeded" NONE" %d\n", tle);
     printf(L_PURPLE"Runtime Error" NONE" %d\n\n", re);
     printf("total time: %dms / %dms (%.2lf%%)\n\n", global_time1, global_time2, (double)global_time1 / global_time2 * 100);
-    printf("" BOLD UNDERLINE "%s" NONE "\n\n", __version.c_str());
+    printf("%s\n\n", __version.c_str());
     //printf(", compiled at %s %s\n", __TIME__, __DATE__);
     start_update();
     exit(0);
@@ -201,14 +201,14 @@ void usage() {
     puts("-q: always quit when error occurs");
     puts("-v: check version and quit");
     puts("-u: update");
-    printf("\n" BOLD UNDERLINE "%s" NONE"%s\n", __version.c_str(), __branch.c_str());
+    printf("\n%s%s\n", __version.c_str(), __branch.c_str());
     printf("compiled at %s %s\n", __TIME__, __DATE__);
     start_update();
     exit(0);
 }
 
 void check_version() {
-    printf(BOLD UNDERLINE "%s" NONE "%s\n", __version.c_str(), __branch.c_str());
+    printf("%s%s\n", __version.c_str(), __branch.c_str());
     printf("compiled at %s %s\n", __TIME__, __DATE__);
     start_update();
     exit(0);
