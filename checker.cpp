46534367e8d299bbab3b22382ad1fd8967b215f8
ycpedef <854261863@qq.com>
2021-02-17 14:14:52 +0800

    2021-02-17 14:14:52

--- checker.cpp
+++ checker.cpp
@@ -19,7 +19,7 @@
 #include "color.h"
 using namespace std;
 
-string version = UNDERLINE "checker v5.6.0" NONE;
+string version = UNDERLINE "zzm ak ioi v5.6.0" NONE;
 string branch = "master";
 
 //string readline(string prompt) {
